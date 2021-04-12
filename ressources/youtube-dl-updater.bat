f27f6a844be1bc200142bdaeb1c8b4806c03664c
dxstiny <dxstiny@users.noreply.github.com>
2021-04-12 12:38:34 +0200

    removed updater.bat

--- ressources/youtube-dl-updater.bat
+++
@@ -1,8 +0,0 @@
-
-@echo off
-echo Waiting for file handle to be closed ...
-ping 127.0.0.1 -n 5 -w 1000 > NUL
-move /Y "C:\Users\David\source\repos\reAudioPlayerML\reAudioPlayerML\bin\Debug\ressources\youtube-dl.exe.new" "C:\Users\David\source\repos\reAudioPlayerML\reAudioPlayerML\bin\Debug\ressources\youtube-dl.exe" > NUL
-echo Updated youtube-dl to version 2021.04.07.
-start /b "" cmd /c del "%~f0"&exit /b"
-                
